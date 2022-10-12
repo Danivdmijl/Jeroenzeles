dbab1eb7221694d64063df07b1b956e509e868d5
unknown <32570@ma-web.nl>
2022-10-12 15:50:35 +0200

    video's jeroen

--- Opdracht 4/js/main.js
+++ Opdracht 4/js/main.js
@@ -1,14 +1,56 @@
 class Subscriber{
 
-    constructor(name) {
-        this.name = name;
+    constructor(number) {
+        console.log("Ik word aangemaakt met: " + number);
+        this.number = number;
+        this.observers = [];
+        console.log("Mijn this.number is nu " + this.getState());
     }
-    init() {
-        console.log("Fakka niffo's");    
+    updateState(newState){
+        console.log("mijn nummer is nu: " + this.getState())
+        this.number = newState
+        console.log("ik update mijn nummer");
+        console.log("mijn nummer is nu: " + this.getState())
+        this.notify();
+    }
+    getState(){
+        return this.number;
+    }
+
+    notify(){
+        console.log("Informing my childs");
+        for(let i = 0; i < this.observers.length; i++){
+            this.observers[i].update();
+        }
+    }
+    
+    attach(observer){
+        this.observers.push(observer);
     }
 }
 
-let x = new Subscriber("Jeroen");
-let y = new Subscriber("Erik");
-console.log(x.name);
-console.log(y.name);
+class Observer{
+    constructor(name, subscriber){
+        this.name = name;
+        this.subscriber = subscriber;
+        this.init();
+    }
+    init(){
+        console.log("Hallo ik ben observer " + this.name + "en de state van mijn sub is nu: " + this.subscriber.getState());
+    }
+    update(){
+        console.log("Hallo ik ben observer " + this.name + "en de state van mijn sub is nu: " + this.subscriber.getState());
+    }
+}
+
+let subscriber__1 = new Subscriber(2);
+
+
+let observer__1= new Observer("Observer 1", subscriber__1);
+subscriber__1.attach(observer__1);
+
+
+let observer__2 = new Observer("Observer 2", subscriber__1);
+subscriber__1.attach(observer__2);
+
+subscriber__1.updateState(100);
