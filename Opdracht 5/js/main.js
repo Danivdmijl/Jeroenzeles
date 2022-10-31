7352936458df7124d8f9622448b8c8f942d30a50
unknown <32570@ma-web.nl>
2022-10-31 19:03:15 +0100

    video 14 afgemaakt

--- Opdracht 5/js/main.js
+++ Opdracht 5/js/main.js
@@ -15,10 +15,29 @@ fetch("./data/trellies.json").then(
         clone.firstElementChild.firstElementChild.innerText = data.title;
         /* number */
         clone.firstElementChild.children[1].children[0].innerText = data.activitiesCount;
-        console.table(data.activities);
-        /* activitie Label */
-        clone.children[1].children[0].children[0].innerText	= data.activities[0].label;
-        /* activitie text */
-        clone.children[1].children[0].children[1].innerText	= data.activities[0].description;
+       
+        let toBeAddedUl = document.createElement("ul");
+        toBeAddedUl.classList.add("tasks__lists");
+        clone.appendChild(toBeAddedUl);
+        
+        for(let i = 0; i < data.activities.length; i++){
+            let toBeAddedLi = document.createElement("li");
+            toBeAddedLi.classList.add("tasks__listItem");
+            toBeAddedUl.appendChild(toBeAddedLi);
+
+            let toBeAddedLabel = document.createElement("label");
+            toBeAddedLabel.classList.add("tasks__label");
+            toBeAddedLabel.innerText = data.activities[i].label;
+            toBeAddedLi.appendChild(toBeAddedLabel);
+
+            let toBeAddedP = document.createElement("p");
+            toBeAddedP.innerText = data.activities[i].description;
+            toBeAddedLi.appendChild(toBeAddedP);
+        }
     }
 )
+
+        // /* activitie Label */
+        // clone.children[1].children[0].children[0].innerText	= data.activities[0].label;
+        // /* activitie text */
+        // clone.children[1].children[0].children[1].innerText	= data.activities[0].description;
