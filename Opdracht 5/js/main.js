b2b40569700f91e65ef0747dd2b8c17a739b9ac7
Danivdmijl <32570@ma-web.nl>
2022-11-01 12:54:55 +0100

    alle videos af

--- Opdracht 5/js/main.js
+++ Opdracht 5/js/main.js
@@ -1,25 +1,46 @@
-const toBeClonedSection = document.getElementById("js--toBeCloned")
+const toBeClonedSection = document.getElementById("js--toBeCloned");
 
-
-fetch("./data/trellies.json").then(
-    function (response) {
+fetch("/data/trellies.json").then(
+    function(response){
         return response.json();
     }
 ).then(
-    function (data) {
-        const clone = toBeClonedSection.cloneNode(true);
-        clone.classList.toggle('tasks--hidden');
-        clone.setAttribute("id","");
-        document.querySelector('main').appendChild(clone);
-        /* h2 */
-        clone.firstElementChild.firstElementChild.innerText = data.title;
-        /* number */
-        clone.firstElementChild.children[1].children[0].innerText = data.activitiesCount;
-       
+    function(data){
+        /* get main */
+        const main = document.querySelector('main');
+        /* make section and add section */
+        let toBeAddedSection = document.createElement("section");
+        toBeAddedSection.classList.add("tasks");
+        main.appendChild(toBeAddedSection);
+        /* make header and add header */
+        let toBeAddedHeader = document.createElement("header");
+        toBeAddedHeader.classList.add("tasks__header");
+        toBeAddedSection.appendChild(toBeAddedHeader);
+        /* make h2 and add h2 */
+        let toBeAddedH2 = document.createElement("h2");
+        toBeAddedH2.classList.add("tasks__h2");
+        toBeAddedH2.innerText = data.title;
+        toBeAddedHeader.appendChild(toBeAddedH2);
+        /* make wrapper and add wrapper */
+        let toBeAddedWrapper = document.createElement("div");
+        toBeAddedWrapper.classList.add("tasks__headerWrapper");
+        toBeAddedHeader.appendChild(toBeAddedWrapper);
+        /* make number and add number */
+        let toBeAddedNumber = document.createElement("div");
+        toBeAddedNumber.classList.add("tasks__number");
+        toBeAddedNumber.innerText = data.activities.length;
+        toBeAddedWrapper.appendChild(toBeAddedNumber);
+        /* make button and add button */
+        let toBeAddedButton = document.createElement("button");
+        toBeAddedButton.classList.add("tasks__edit");
+        toBeAddedButton.innerText = "..."
+        toBeAddedWrapper.appendChild(toBeAddedButton);
+        /* make ul and add ul */
         let toBeAddedUl = document.createElement("ul");
-        toBeAddedUl.classList.add("tasks__lists");
-        clone.appendChild(toBeAddedUl);
-        
+        toBeAddedUl.classList.add("tasks__list");
+        toBeAddedSection.appendChild(toBeAddedUl);
+
+
         for(let i = 0; i < data.activities.length; i++){
             let toBeAddedLi = document.createElement("li");
             toBeAddedLi.classList.add("tasks__listItem");
@@ -27,17 +48,26 @@ fetch("./data/trellies.json").then(
 
             let toBeAddedLabel = document.createElement("label");
             toBeAddedLabel.classList.add("tasks__label");
+            if(data.activities[i].extraClass !== ""){
+                toBeAddedLabel.classList.add("tasks__label--" + data.activities[i].extraclass)
+            }
             toBeAddedLabel.innerText = data.activities[i].label;
             toBeAddedLi.appendChild(toBeAddedLabel);
 
-            let toBeAddedP = document.createElement("p");
-            toBeAddedP.innerText = data.activities[i].description;
-            toBeAddedLi.appendChild(toBeAddedP);
+            let toBeAddedParagraph = document.createElement("p");
+            toBeAddedParagraph.innerText = data.activities[i].description;
+            toBeAddedLi.appendChild(toBeAddedParagraph);
+
+            let toBeAddedContainer = document.createElement("div");
+            toBeAddedContainer.classList.add("tasks__container");
+            toBeAddedLi.appendChild(toBeAddedContainer);
+
+            let toBeAddedAuthor = document.createElement("p");
+            if(data.activities[i].author !== ""){
+                toBeAddedAuthor.innerText = data.activities[i].author;
+                toBeAddedAuthor.classList.add("tasks__author");
+            }
+            toBeAddedContainer.appendChild(toBeAddedAuthor);
         }
     }
 )
-
-        // /* activitie Label */
-        // clone.children[1].children[0].children[0].innerText	= data.activities[0].label;
-        // /* activitie text */
-        // clone.children[1].children[0].children[1].innerText	= data.activities[0].description;
